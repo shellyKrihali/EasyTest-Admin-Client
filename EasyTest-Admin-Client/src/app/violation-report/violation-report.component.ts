7297462a8e0170dc768adee5ade07149e9e74c05
baro <bar.o@takoomi.com>
2021-06-05 10:55:23 +0300

    admit update

--- EasyTest-Admin-Client/src/app/violation-report/violation-report.component.ts
+++ EasyTest-Admin-Client/src/app/violation-report/violation-report.component.ts
@@ -8,36 +8,58 @@ import { CourseService } from '../services/course.service';
   styleUrls: ['./violation-report.component.css']
 })
 export class ViolationReportComponent implements OnInit {
-  reports:any[]= [];
-  courses:any[] = [];
+  reports: any[] = [];
+  courses: any[] = [];
   apps = [];
-  courseAppId;
+  selectedApp
   constructor(private courseAppsService: CourseAppService, private courseService: CourseService) { }
 
   ngOnInit(): void {
     this.getAllCoursesAndAppearances();
   }
 
-  private getAllCoursesAndAppearances() {
+  getAllCoursesAndAppearances() {
     this.courseService.getAllCourses().toPromise().then(res => {
       console.log(res);
-      this.courses= res.courses;
+      this.courses = res.courses;
       this.courses.forEach(element => {
         console.log(element);
-        
+
         element.appearances.forEach(app => {
           console.log(app);
+
           this.apps.push(app);
+
         });
       });
 
- 
+
     })
   }
 
-  private getAllReportData() {
-      this.courseAppsService.getCourseAppReport(this.courseAppId).subscribe(res =>{
-        this.reports=res;
+  reloadTable() {
+
+    if (this.selectedApp != undefined) {
+      this.courseAppsService.getCourseAppReport(this.selectedApp._id).toPromise().then(res => {
+        console.log(res)
+        res.students.forEach(element => {
+          var new_dates = [];
+          element.loginDates.forEach(date => {
+            var temp_date = new Date(date)
+            var min = temp_date.getHours();
+            var hr = temp_date.getMinutes();
+            date = min + ":" + hr
+            console.log(min)
+            new_dates.push(date)
+
+          });
+          res.students[res.students.indexOf(element)].loginDates = new_dates
+
+
+        });
+        this.reports = res.students;
+
       })
+    }
   }
 }
